0d25d544e261fd20514b908c571073678fd1f4ce
Ivaylo Ivanov <ivaylozivanov1@gmail.com>
2021-12-18 10:06:23 +0200

    Solution to part 1

---
+++ 2021/day 1/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+//Part 1
+function largerMeasurementFinder(file) {
+  fs.readFile(__dirname + file, (err, data) => {
+    let counter = 0;
+
+    const parsedData = data
+      .toString()
+      .replace(/(\r\n|\n|\r)/gm, " ")
+      .split(" ");
+
+    for (let i = 1; i < parsedData.length; i++) {
+      if (Number(parsedData[i]) > Number(parsedData[i - 1])) {
+        counter += 1;
+      }
+    }
+
+    console.log(counter);
+  });
+}
+
+largerMeasurementFinder("/input.txt");
